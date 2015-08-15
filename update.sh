421d76af536943dd3fbc489d2e720ca26e0ca028
Edward Muller <edwardam@interlix.com>
2015-08-14 18:38:12 -0700

    Auto-generate versions?
    Kinda.

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+echo "${versions[@]}"
+
+for version in "${versions[@]}"; do
+   (
+		 set -x
+		 < Dockerfile sed -E 's/^(ENV GOVERSION) .*/\1 '"$version"'/' > $version/Dockerfile
+		 cp compile $version/
+     cp go-docker.sh $version/
+  )
+done
